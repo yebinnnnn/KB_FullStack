4404fdc712f10b030679e9bb70dfca99c7d0b2f8
yebinnnnn <prettybird1@naver.com>
2025-03-13 17:22:37 +0900

    Node.js Sync

--- 02_NODE/async/1.async.js
+++ 02_NODE/async/1.async.js
@@ -9,16 +9,19 @@ let showTime = (msg) => {
 showTime('');
 
 // 3초 후 실행
+//setTimeout 함수는 비동기 함수임. 그 간극동안 다음 작업을 하러간다.
 setTimeout(() => {
   showTime('1 : 3초후');
 }, 3000);
 
 // 5초 후 실행
 setTimeout(() => {
+  //앞의 3초동안 두번째 작업인 여기로 와서 실행하려함
   showTime('2 : 5초후');
 }, 5000);
 
 // 1초 후 실행
 setTimeout(() => {
+  //그럼 5초사이 이 작업을 하러 도착.
   showTime('3 : 1초후');
 }, 1000);
