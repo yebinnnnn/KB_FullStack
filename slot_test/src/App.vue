81d0734fe91699b76bd93195e80e4cb894779689
yebinnnnn <prettybird1@naver.com>
2025-03-26 15:29:05 +0900

    slot_test 추가. Vue slot관련 실습

---
+++ slot_test/src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import CheckboxItem from './components/CheckboxItem.vue';
+export default {
+  name: 'App',
+  components: { CheckboxItem },
+  data() {
+    return {
+      checkboxData: 'ONE',
+    };
+  },
+};
+</script>
+
+<template>
+  <p>슬롯 : 부모컴포넌트에서 자식 컴포넌트로 정보를 전달하는 방법이다.</p>
+  <ul>
+    <!-- li요소를 자식 컴포넌트로 옮겨보자! -->
+    <!-- <li><input type="checkbox" /><span>ONE</span></li> -->
+    <CheckboxItem :checkboxData="checkboxData">
+      <template v-slot:default>
+        <span>{{ checkboxData }}</span>
+        <span>{{ checkboxData }}</span>
+        <span>{{ checkboxData }}</span>
+      </template>
+      <template v-slot:icon>
+        <img :src="checkboxData" alt="" />
+      </template>
+    </CheckboxItem>
+  </ul>
+</template>
+
+<style scoped></style>
---
+++ slot_test/src/components/CheckboxItem.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  //name 옵션. props 옵션 등으로 불린다.
+  name: 'CheckboxItem',
+  props: {
+    checkboxData: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    this.data = this.CheckboxData + '입니다.';
+  },
+};
+</script>
+<template>
+  <!-- <li>
+    <input type="checkbox" /><span>{{ data }}</span>
+  </li> -->
+  <!-- 늘어나면 유지보수하기 어려움. 부모쪽에서 전달해야 할  props 가 너무 많다 -->
+  <li>
+    <!--
+    <input type="checkbox" />
+    <span>{{ data }}</span>
+    <span>{{ data }}</span>
+    <span>{{ data }}</span> -->
+    <input type="checkbox" />
+    <!-- 속성값 뿐만 아니라 템플릿 자체를 전달할때 slot 이 사용됨. -->
+    <slot></slot>
+    <hr />
+    <slot name="icon"></slot>
+  </li>
+</template>
+<style></style>
