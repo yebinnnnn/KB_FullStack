240a97504373ec04763cb944947f22a58bfc3fb5
yebinnnnn <prettybird1@naver.com>
2025-03-27 15:21:32 +0900

    Vue: setup함수 공부용

--- 09_compositionAPI/setup_test/src/App.vue
+++ 09_compositionAPI/setup_test/src/App.vue
@@ -1,12 +1,60 @@
 <script>
-import { ref, reative } from 'vue';
+import { ref, reactive } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 export default {
+  components: { HelloWorld },
+  onMounted: {},
   setup() {
+    //create() 역할, data()+methods+ computed
     const refNum = ref(10);
+    refNum.value = 20;
+
+    const refObj = ref({ title: 'aaa' });
+    refObj.value.title = 'BBB';
+
+    const refArr = ref([]);
+    //const 로 선언되어있어서 값을 변경할 수 없으므로 값 자체가 아니라 value 로 접근
+    const source = [10, 20, 30];
+    // refArr.value = source;
+    // refArr.value[0] = 111;
+    // console.log(source);
+    // 헐~ source 값까지 변경이됨. 콜바이레퍼런스 형태의 참조
+    //메모리 어떻게 그리는지 살펴보기
+
+    Object.assign(refArr.value, source); //이게 객체 내용을 복붙하는거~
+    refArr.value[0] = 200;
+    console.log(source);
+
+    // reactive. 객체를 반환한다.
+    const reactiveObj = reactive({ title: 'Hello' });
+    reactiveObj.title = 'KB';
+
+    const reactiveArr = reactive([]);
+    Object.assign(reactiveArr, source);
+    reactiveArr[0] = 111;
+    console.log(source);
+
+    const parentMsg = 'HELLOWORLD';
+    return { refNum, refObj, refArr, reactiveObj, reactiveArr, parentMsg };
   },
 };
 </script>
 
-<template></template>
+<template>
+  <HelloWorld :childMsg="parentMsg" />
+  <ul>
+    <li>{{ refNum }}</li>
+    <input type="text" v-model="refNum" />
+    <li>
+      {{ refObj }}
+    </li>
+    <input type="text" v-model="refObj.title" />
+    <li>{{ refArr }}</li>
+    <input type="text" v-model.number="refArr[1]" />
+    <li>{{ reactiveObj }}</li>
+    <input type="text" v-model="reactiveObj.title" />
+    <li>{{ reactiveArr }}</li>
+  </ul>
+</template>
 
 <style scoped></style>
--- 09_compositionAPI/setup_test/src/components/HelloWorld.vue
+++ 09_compositionAPI/setup_test/src/components/HelloWorld.vue
@@ -1,23 +1,30 @@
-<script setup>
+<!-- <script setup>
 defineProps({
   msg: {
     type: String,
     required: true,
   },
 })
+</script> -->
+<script>
+export default {
+  props: {
+    childMsg: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props, context) {
+    const msg = props.childMsg;
+    return { msg };
+  },
+};
 </script>
-
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
   </div>
 </template>
-
 <style scoped>
 h1 {
   font-weight: 500;
@@ -25,16 +32,13 @@ h1 {
   position: relative;
   top: -10px;
 }
-
 h3 {
   font-size: 1.2rem;
 }
-
 .greetings h1,
 .greetings h3 {
   text-align: center;
 }
-
 @media (min-width: 1024px) {
   .greetings h1,
   .greetings h3 {
