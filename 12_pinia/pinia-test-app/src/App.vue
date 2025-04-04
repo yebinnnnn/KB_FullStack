e4d4b0e3424188a5e007fbea9ba736624c4c61b9
yebinnnnn <prettybird1@naver.com>
2025-04-04 09:52:59 +0900

    pinia 실습 시작

---
+++ 12_pinia/pinia-test-app/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import TheWelcome from './components/TheWelcome.vue';
+import { useCounterStore } from './stores/counter';
+import { computed } from 'vue';
+
+const store = useCounterStore();
+const { increment } = store;
+const count = computed(() => store.count);
+
+console.log(store);
+increment();
+</script>
+
+<template>
+  {{ store.count }}{{ store.doubleCount }}<br />
+  {{ count }}
+</template>
+
+<style scoped></style>
