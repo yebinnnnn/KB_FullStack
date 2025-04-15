390101a5681352d8e093ccfdec2f469b949d32a0
yebinnnnn <prettybird1@naver.com>
2025-04-15 15:45:26 +0900

    0415수업

---
+++ 04_java/study/src/ch01/Loop.java
@@ -0,0 +1,10 @@
+package ch01;
+
+public class Loop {
+    public static void main(String[] args){
+        int al=-1;
+        while(al++<25){
+            System.out.println((char)('A'+al));
+        }
+    }
+}
